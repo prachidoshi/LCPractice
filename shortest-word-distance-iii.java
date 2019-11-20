fd6cf4df8f0a6ed6492a2eae1ab3990089aef392
Prachi Doshi <prachidoshi@users.noreply.github.com>
2019-11-19 17:03:54 -0800

    Create shortest-word-distance-iii.java

---
+++ shortest-word-distance-iii.java
@@ -0,0 +1,36 @@
+/*
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+word1 and word2 may be the same and they represent two individual words in the list.
+
+Example:
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Input: word1 = “makes”, word2 = “coding”
+Output: 1
+Input: word1 = "makes", word2 = "makes"
+Output: 3
+Note:
+You may assume word1 and word2 are both in the list.
+*/
+
+class Solution {
+    public int shortestWordDistance(String[] words, String word1, String word2) {
+        long dist = Integer.MAX_VALUE, i1 = dist, i2 = -dist;
+    boolean same = word1.equals(word2);
+    for (int i=0; i<words.length; i++) {
+        if (words[i].equals(word1)) {
+            if (same) {
+                i1 = i2;
+                i2 = i;
+            } else {
+                i1 = i;
+            }
+        } else if (words[i].equals(word2)) {
+            i2 = i;
+        }
+        dist = Math.min(dist, Math.abs(i1 - i2));
+    }
+    return (int) dist;
+    }
+}
