f3b770a209a56c1c7790b9a0eb954580ba391991
Prachi Doshi <prachidoshi@users.noreply.github.com>
2019-11-19 19:35:48 -0800

    Create sparse-matrix-multiplication.java

---
+++ sparse-matrix-multiplication.java
@@ -0,0 +1,60 @@
+/*
+Given two sparse matrices A and B, return the result of AB.
+
+You may assume that A's column number is equal to B's row number.
+
+Example:
+
+Input:
+
+A = [
+  [ 1, 0, 0],
+  [-1, 0, 3]
+]
+
+B = [
+  [ 7, 0, 0 ],
+  [ 0, 0, 0 ],
+  [ 0, 0, 1 ]
+]
+
+Output:
+
+     |  1 0 0 |   | 7 0 0 |   |  7 0 0 |
+AB = | -1 0 3 | x | 0 0 0 | = | -7 0 3 |
+                  | 0 0 1 |
+*/
+class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+         int m = A.length, n = A[0].length, nB = B[0].length;
+    int[][] result = new int[m][nB];
+
+    List[] indexA = new List[m];
+    for(int i = 0; i < m; i++) {
+        List<Integer> numsA = new ArrayList<>();
+        for(int j = 0; j < n; j++) {
+            if(A[i][j] != 0){
+                numsA.add(j); 
+                numsA.add(A[i][j]);
+            }
+        }
+        indexA[i] = numsA;
+    }
+
+    for(int i = 0; i < m; i++) {
+        List<Integer> numsA = indexA[i];
+        for(int p = 0; p < numsA.size() - 1; p += 2) {
+            int colA = numsA.get(p);
+            int valA = numsA.get(p + 1);
+            for(int j = 0; j < nB; j ++) {
+                int valB = B[colA][j];
+                result[i][j] += valA * valB;
+            }
+        }
+    }
+
+    return result;   
+    }
+
+   
+}
